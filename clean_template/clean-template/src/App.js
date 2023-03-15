6bf851315f01c6deecfaa765c336711a6c3ce913
rafapm1999 <rportillom99.code@gmail.com>
2023-03-15 18:20:52 +0100

    creo clean-template

---
+++ clean_template/clean-template/src/App.js
@@ -0,0 +1,12 @@
+//import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab eum impedit consectetur placeat. Delectus doloremque quisquam in facere deleniti quia.</p>
+    </div>
+  );
+}
+
+export default App;
